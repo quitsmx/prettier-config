d0356f43e612f1f67407ff30c336612a660fe547
aMarCruz <amarcruz.mx@gmail.com>
2025-08-13 00:45:54 -0600

    add types in separate file

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'prettier'
+
+/**
+ * Prettier configuration for QuITS.
+ *
+ * Rules that use their default value are commented out.
+ *
+ * @see https://prettier.io/docs/configuration
+ */
+declare const prettierConfig: Config
+export default prettierConfig
